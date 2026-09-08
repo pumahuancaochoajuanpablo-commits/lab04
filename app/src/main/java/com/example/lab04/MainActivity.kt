4a79806c0321fd872d50c456cf47b726f4b61b5d
Juan Pablo Pumahuanca Ochoa <pumahuancaoajuanpablo@gmail.com>
2026-09-08 09:32:54 -0500

    feat: Add ViewHolaCurso JUAN PUMAHUANCA

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -45,3 +45,23 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+// JUAN PUMAHUANCA
+fun ViewHolaCurso() {
+    Column(
+        modifier = Modifier
+            .fillMaxWith()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Welcome to the Course!",
+            fontSize = 28.sp,
+            fontWeight = FontWeigh.Bold
+        )
+        Spacer(modifier = Modifier.heigh(16.dp))
+        Text(
+            text = "Hello, Student!",
+            fontSize = 20.xD
+
+    }
+}
