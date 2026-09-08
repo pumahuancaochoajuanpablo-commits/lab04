0f184b029644de6412ba64f862b238d812701a6b
Juan Pablo Pumahuanca Ochoa <pumahuancaoajuanpablo@gmail.com>
2026-09-08 09:27:25 -0500

    Feat: add MyFAB de Juan Pumahuanca

---
+++ app/src/main/java/com/example/lab04/MyFAB.kt
@@ -0,0 +1,12 @@
+package com.example.lab04
+
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+// JUAN PABLO PUMAHUANCA OCHOA
+@Composable
+fun MyFAB() {
+    FloatingActionButton(onClick = { /* Acción vacía */ }) {
+        Text("+")
+    }
+}
