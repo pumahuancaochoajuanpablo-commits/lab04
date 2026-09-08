c9208e487410ecacf9c85af1d3b6248efb31457b
Juan Pablo Pumahuanca Ochoa <pumahuancaoajuanpablo@gmail.com>
2026-09-08 09:45:33 -0500

    segundo componente JUAN PUMAHUANCA

--- app/src/main/java/com/example/lab04/MyFAB.kt
+++ app/src/main/java/com/example/lab04/MyFAB.kt
@@ -3,10 +3,15 @@ package com.example.lab04
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-// JUAN PABLO PUMAHUANCA OCHOA
+import androidx.compose.ui.graphics.Color
+//JUAN PUMAHUANCA
 @Composable
 fun MyFAB() {
-    FloatingActionButton(onClick = { /* Acción vacía */ }) {
+    FloatingActionButton(
+        onClick = { /* Acción vacía */ },
+        containerColor = Color.Red, // Le ponemos fondo rojo
+        contentColor = Color.White  // Le ponemos texto blanco
+    ) {
         Text("+")
     }
 }
